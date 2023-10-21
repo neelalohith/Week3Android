ce2997120025078051ea65263bd995a3f6832750
Neelalohith R Kashyap <neelalohithhp2002@gmail.com>
2023-10-21 18:57:06 +0530

    Update Employee.kt

--- app/src/main/java/com/example/week2android/Employee.kt
+++ app/src/main/java/com/example/week2android/Employee.kt
@@ -1,4 +1,30 @@
 package com.example.week2android
-
 class Employee(val employeeId: Int, val employeeName:String) {
+}
+fun main(){
+    //val emp = Employee(1,"s")
+    val emp: Employee?
+    null.also { emp = it }
+    if(emp == null){
+        println("emp is null")
+    }
+    else{
+        println(emp?.employeeId)
+        println(emp?.employeeName)
+        println(emp!!.employeeId)
+        println(emp!!.employeeName)
+    }
+    fun performTask(callback: Callback) {
+        // Simulating some task
+        val result = "Task completed successfully"
+        callback(result)
+    }
+    val result = Driver.performOperation(10, 5) { a, b ->
+        a - b
+    }
+    println("Result of subtraction: $result") // Output: R
+    fun performOperation(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
+        return operation(a, b)
+    }
+
 }
